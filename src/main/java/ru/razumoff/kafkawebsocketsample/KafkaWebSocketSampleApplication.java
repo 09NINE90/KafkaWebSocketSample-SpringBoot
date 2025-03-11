fc3fd547cf8649e772fb3d1ed29728ba6ababb4f
09NINE90 <razumov.aleksei101@yandex.ru>
2025-03-11 10:26:33 +0500

    init

---
+++ src/main/java/ru/razumoff/kafkawebsocketsample/KafkaWebSocketSampleApplication.java
@@ -0,0 +1,13 @@
+package ru.razumoff.kafkawebsocketsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWebSocketSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWebSocketSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/razumoff/kafkawebsocketsample/KafkaWebSocketSampleApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.razumoff.kafkawebsocketsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWebSocketSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assert true;
+    }
+
+}
